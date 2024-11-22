45cfd5c55f21fb7f98765f5f820207c326614c7e
Parthiban S <56575076+Parthyapps@users.noreply.github.com>
2024-11-22 08:20:14 +0530

    Contains Duplicate.kt
    updated the problem

--- Contains Duplicate.kt
+++ Contains Duplicate.kt
@@ -1,11 +1,19 @@
   fun main() {
+    // intArrayOf is a Kotlin function used to create an array of integers.
       val array = intArrayOf(1,2,3,4)
       val result = containsDuplicate(array)
       println(result)
   }
   fun containsDuplicate(array: IntArray):Boolean{
+   // Iterates through each index of the array.
+    // array.indices generates a range representing all valid indices of the array (e.g., for an array of size 4, indices is 0..3).
       for(i in array.indices){
+            // For each index i, starts a second loop from index i + 1 up to (but not including) array.size.
+            // Ensures that each element in the array is compared only with the elements that come after it (avoids redundant comparisons).
           for (j in i+1 until array.size){
+            
+    // Compares the element at index i with the element at index j.
+    // If the elements are equal, it means the array contains duplicate values.
               if(array[i] == array[j]){
                   return true
               }
